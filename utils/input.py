7dacb6fb8611638aa089554c8faa001c5e5f3b5a
Jonathan Tay <jonathantaymy@gmail.com>
2023-12-17 15:07:37 +0800

    feat(utils): retrieve input when input file is blank

--- utils/input.py
+++ utils/input.py
@@ -12,13 +12,15 @@ def aoc(year, day):
     if not token and not os.path.isfile(filename):
         raise Exception(f'SESSION_TOKEN not found in .env file and manual input in {filename} not found.')
 
-    if not os.path.isfile(filename):
-        req = requests.get(url, cookies={'session': token}, headers={
-            'User-Agent': "https://github.com/jontmy/aoc-python/blob/master/utils/input.py by jontmy"})
-        text = req.text.strip()
-        with open(filename, 'w') as f:
-            f.write(text)
-        return text
-
     with open(filename) as f:
-        return f.read().strip()
+        curr = f.read().strip()
+
+    if curr and not curr.startswith("Please don't repeatedly request this endpoint before it unlocks!"):
+        return curr
+
+    req = requests.get(url, cookies={'session': token}, headers={
+        'User-Agent': "https://github.com/jontmy/aoc-python/blob/master/utils/input.py by jontmy"})
+    curr = req.text.strip()
+    with open(filename, 'w') as f:
+        f.write(curr)
+    return curr
